e658cb16eb2ae128801fae2a6a869d8ea8f66ce5
popovre <popov.roman.e@gmail.com>
2024-02-01 15:18:46 +0500

    made hw/4 form

---
+++ src/components/review-form/component.jsx
@@ -0,0 +1,102 @@
+import { useReducer } from 'react';
+import styles from './style.module.scss';
+import classNames from 'classnames';
+
+const INITIAL_FORM = {
+  name: '',
+  text: '',
+  rating: 5,
+};
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case 'setName':
+      return {
+        ...state,
+        name: payload,
+      };
+    case 'setText':
+      return {
+        ...state,
+        text: payload,
+      };
+    case 'setRating':
+      return {
+        ...state,
+        rating: payload,
+      };
+    case 'setMadRating':
+      return {
+        ...state,
+        name: 'э, поставь повыше!',
+        rating: +payload + 2,
+      };
+    case 'setMadRatingToo':
+      return {
+        ...INITIAL_FORM,
+        name: 'э, поставь повыше!',
+        text: 'сказано же...',
+        rating: +payload + 1,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+const ReviewForm = () => {
+  const [form, dispatch] = useReducer(reducer, INITIAL_FORM);
+
+  const onChangeRating = (e) => {
+    return +e.target.value === 1
+      ? dispatch({ type: 'setMadRating', payload: e.target.value })
+      : +e.target.value === 2
+      ? dispatch({ type: 'setMadRatingToo', payload: e.target.value })
+      : dispatch({ type: 'setRating', payload: e.target.value });
+  };
+
+  return (
+    <div className={styles.root}>
+      <h3>Write your review</h3>
+      <div className={styles.field}>
+        <label htmlFor="name">Name</label>
+        <input
+          className={classNames(styles.input)}
+          id="name"
+          type="text"
+          value={form.name}
+          onChange={(e) =>
+            dispatch({ type: 'setName', payload: e.target.value })
+          }
+        />
+      </div>
+      <div className={styles.field}>
+        <label htmlFor="text">Text</label>
+        <input
+          className={classNames(styles.input)}
+          id="text"
+          type="text"
+          value={form.text}
+          onChange={(e) =>
+            dispatch({ type: 'setText', payload: e.target.value })
+          }
+        />
+      </div>
+      <div className={styles.field}>
+        <label htmlFor="rating">Rating</label>
+        <input
+          className={classNames(styles.input)}
+          id="rating"
+          type="number"
+          min="0"
+          max="5"
+          value={form.rating}
+          onChange={onChangeRating}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ReviewForm;
