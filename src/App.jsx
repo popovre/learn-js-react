296844e9b79c89b6ff01edc1635a8e30d8d85580
Roman <popov.roman.e@gmail.com>
2024-02-13 12:13:44 +0500

    resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import Layout from './components/layout/component';
-import { restaurants } from './constants/mock';
 import Restaurants from './components/restaurants/component';
 import { Provider } from 'react-redux';
 import { store } from './redux';
+import { UserContext } from './contexts/user';
+import { user as USER_DEFAULT } from './constants/structures';
+import { useState } from 'react';
 
 const App = () => {
+  const [user, setUser] = useState(USER_DEFAULT);
   return (
     <Provider store={store}>
-      <Layout>
-        <Restaurants />
-      </Layout>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Layout>
+          <Restaurants />
+        </Layout>
+      </UserContext.Provider>
     </Provider>
   );
 };
