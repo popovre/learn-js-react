8a077f389671a0685bad913fee01d039455b081c
Roman <popov.roman.e@gmail.com>
2024-02-20 00:45:49 +0500

    made condition

--- src/redux/entities/user/thunks/get-users.jsx
+++ src/redux/entities/user/thunks/get-users.jsx
@@ -1,22 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-// import { selectRestaurantUsersById } from '../../restaurant/selectors';
-// import { selectUserIds } from '../selectors';
+import { selectUserIds } from '../selectors';
 
 export const getUsers = createAsyncThunk(
   '/api/getUsersByRestaurantId',
   async () => {
     const response = await fetch(`http://localhost:3001/api/users`);
     return await response.json();
-  }
-  // {
-  //   condition: (userId, { getState }) => {
-  //     const restaurantUserIds = selectRestaurantUsersById(
-  //       getState(),
-  //       restaurantId
-  //     );
-
-  //     const userIds = selectUserIds(getState());
-  //     return !restaurantUserIds.every((id) => userIds.includes(id));
-  //   },
-  // }
+  },
+  { condition: (_, { getState }) => !selectUserIds(getState())?.length }
 );
