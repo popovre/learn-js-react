968fd863c510b4478751ed8cad711d480660f8ca
Roman <popov.roman.e@gmail.com>
2024-02-21 01:32:56 +0500

    request api

--- src/redux/index.js
+++ src/redux/index.js
@@ -5,14 +5,21 @@ import { reviewSlice } from './entities/review';
 import { userSlice } from './entities/user';
 import { requestSlice } from './ui/request';
 import { cartSlice } from './ui/cart';
+import { api } from './services/api';
+import { buildGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
 
 export const store = configureStore({
-  reducer: combineSlices(
-    restaurantSlice,
-    dishSlice,
-    reviewSlice,
-    userSlice,
-    requestSlice,
-    cartSlice
-  ),
+  reducer: {
+    ...combineSlices(
+      restaurantSlice,
+      dishSlice,
+      reviewSlice,
+      userSlice,
+      requestSlice,
+      cartSlice
+    ),
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
---
+++ src/redux/services/api.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/api/' }),
+  endpoints: (builder) => ({
+    getRestaurants: builder.query({
+      query: () => ({ url: 'restaurants' }),
+    }),
+    getUsers: builder.query({
+      query: () => ({ url: 'users' }),
+    }),
+    getDishes: builder.query({
+      query: (restaurantId) => ({ url: 'dishes', params: { restaurantId } }),
+    }),
+    getReviews: builder.query({
+      query: (restaurantId) => ({ url: 'reviews', params: { restaurantId } }),
+    }),
+  }),
+});
+
+export const { getRestaurantsQuery, getDishesQuery, getReviewsQuery } = api;
