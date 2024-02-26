500e47e203aed61ceb7ecb125587d8714af5fcc5
popovre <popov.roman.e@gmail.com>
2024-02-26 18:15:23 +0500

    starts update review form

--- src/components/update-review-form/container.jsx
+++ src/components/update-review-form/container.jsx
@@ -1,7 +1,39 @@
 import ReviewForm from '../review-form/component';
+import { useUpdateReviewMutation } from '../../redux/services/api';
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/user';
+import { useEffect } from 'react';
 
-const UpdateReviewForm = () => {
-  return <ReviewForm />;
+const UpdateReviewFormContainer = ({ review, user, onUpdateFinished }) => {
+  const [updateReview, { isLoading, isSuccess }] = useUpdateReviewMutation();
+
+  useEffect(() => {
+    if (isSuccess) {
+      onUpdateFinished();
+    }
+  }, [onSuccess, onUpdateFinished]);
+
+  if (!user || !review) {
+    return null;
+  }
+
+  if (isLoading) {
+    return <div>Updating review...</div>;
+  }
+
+  const { id, text, rating } = review;
+  return (
+    <ReviewForm
+      initialState={{ text, rating }}
+      user={user?.name}
+      onSave={(updateReview) =>
+        updateReview({
+          reviewId: id,
+          newReview: { ...updateReview, userId: user.id, id: id },
+        })
+      }
+    />
+  );
 };
 
-export default UpdateReviewForm;
+export default UpdateReviewFormContainer;
